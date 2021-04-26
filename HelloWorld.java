3db895601c6d0af7b4e4e11fbf5825cdd53604ee
learningcloud9 <82770639+learningcloud9@users.noreply.github.com>
2021-04-26 10:02:26 +0100

    Add files via upload - test check
    For Demo purpose only

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package com.learningcloud9;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 10;
+        int b = 20;
+		System.out.println("sum of a and b is ="+(a+b));
+	}
+
+}
